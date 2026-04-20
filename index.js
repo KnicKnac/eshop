95fd37e636cf6542fad8d24247c08ad490e4846c
Eimantas Mitrikas <eimantas.mit@gmail.com>
2026-04-20 21:07:49 +0300

    Javascript

---
+++ index.js
@@ -0,0 +1,31 @@
+    
+    
+    console.log("Hello World!");
+   
+    function remove(){
+        document.getElementById("cart-item").innerHTML= "";
+    }
+
+
+    const string = "Hello World";
+    console.log('String:', string);
+    const number = 3.14;
+    console.log('Number:', number);
+    const boolean = true;
+    console.log('Boolean:', boolean);
+    const nullValue = null;
+    console.log('NullValue:', nullValue);
+    const undefinedValue = undefined;
+    console.log('Undefined:', undefinedValue);
+    const NaNValue = NaN;
+    console.log('NaNValue:', NaNValue);
+    const array = [1, 2, 3];
+    console.log('Array:', array);
+    const object = {
+        name: 'John',
+        age: 30,
+        isStudent = true,
+        hobbies: ['readieng', 'coding', 'gaming'],
+    }
+    console.log('Object:', object);
+    
